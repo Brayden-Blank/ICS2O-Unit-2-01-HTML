293bc01d71036f9a17cb39811b39e5e1adf48a69
Brayden-Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-03-09 14:45:22 +0000

    added javascript

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Created by: Brayden Blank
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // This function displays "Hello, World!"
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
